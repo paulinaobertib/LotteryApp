05d819f91a6730a06ce26a6c872fe33305f86162
Pau Oberti <paulinaobertibusso@gmail.com>
2024-11-04 14:55:44 -0300

    initialization

---
+++ ms-lottery/src/main/java/app/ms_lottery/MsLotteryApplication.java
@@ -0,0 +1,13 @@
+package app.ms_lottery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsLotteryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsLotteryApplication.class, args);
+	}
+
+}
